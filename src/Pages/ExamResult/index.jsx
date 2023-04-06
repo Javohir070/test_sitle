a09b8370086923c37f84fdee47c503287109f81c
fazliddin-web79 <94189550+fazliddin-web79@users.noreply.github.com>
2023-04-06 10:01:12 +0500

    Update index.jsx
    yaxshi ishlayapti test

--- src/Pages/ExamResult/index.jsx
+++ src/Pages/ExamResult/index.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React , {useEffect} from "react";
 import { useNavigate } from "react-router-dom";
 import { Container } from "./styled";
 
 const ExamResult = () => {
   const navigate = useNavigate();
-
+useEffect(()=>{
+if(localStorage.getItem("resultExam") * 100 <= 60){
+  navigate("/lectures")
+}
+},[])
   return (
     <Container>
       <h1>Ваш результат экзамена</h1>
